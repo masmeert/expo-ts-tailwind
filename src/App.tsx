efce0918be58ee89ba911e83ee3c013fe98a15ff
Massimo Meert <pro@masmeert.com>
2023-03-20 17:46:13 +0100

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
@@ -7,7 +8,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,37 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 import ImageViewer from "../components/ImageViewer";
 import Button from "../components/Button";
 const PlaceholderImage = require("../../assets/images/bg.png");
 
-export default function App() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
+    <View className="flex-1 bg-gray-900 items-center">
+      <View className="mt-16">
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
-      <View style={styles.footerContainer}>
+      <View className="mt-16">
         <Button theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: "center",
-  },
-});
