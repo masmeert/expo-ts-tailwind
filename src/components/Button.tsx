b64f1e2b51b8b38029c2f4187f12fe3e36973a6f
Massimo Meert <massimomeert@proton.me>
2023-03-20 18:37:53 +0100

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,30 +7,28 @@ type Props = {
   theme?: "primary";
 };
 
-const buttonContainerStyle = "w-80 h-16 mx-20 items-center justify-center p-[3px]";
-
 export default function Button({ label, theme }: Props) {
   if (theme === "primary") {
     return (
-      <View className={clsx(buttonContainerStyle, "border-4 border-yellow-400 rounded-2xl")}>
-        <Pressable className={clsx(_styles.button, "bg-white")} onPress={() => alert("You pressed a button.")}>
+      <View className={clsx(styles.buttonContainer, "border-4 border-yellow-400 rounded-2xl")}>
+        <Pressable className={clsx(styles.button, "bg-white")} onPress={() => alert("You pressed a button.")}>
           <FontAwesome name="picture-o" size={18} color="#25292e" style={{ paddingRight: 8 }} />
-          <Text className={clsx(_styles.buttonLabel, "text-[#25292e] font-bold")}>{label}</Text>
+          <Text className={clsx(styles.buttonLabel, "text-[#25292e] font-bold")}>{label}</Text>
         </Pressable>
       </View>
     );
   }
 
   return (
-    <View className={_styles.buttonContainer}>
-      <Pressable className={_styles.button} onPress={() => alert("You pressed a button.")}>
-        <Text className={_styles.buttonLabel}>{label}</Text>
+    <View className={styles.buttonContainer}>
+      <Pressable className={styles.button} onPress={() => alert("You pressed a button.")}>
+        <Text className={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
   );
 }
 
-const _styles = {
+const styles = {
   buttonContainer: "w-80 h-16 mx-20 p-[3px]",
   button: "rounded-xl w-full h-full items-center justify-center flex-row",
   buttonLabel: "text-white text-[16px]",
